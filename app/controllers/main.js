f5420a736341806d982c40299b2f201aeb17a83e
Juan-Manuel Fluxà <jmfluxa@gmail.com>
2015-04-19 23:19:56 +0200

    prod

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -11,8 +11,8 @@ exports.index = function (req, res) {
 
 exports.capture = function(req, res) {
 	// test local
-	res.json({success: true, filename: 'test.jpg', ts: Math.random()*99999999})
-	return;
+	// res.json({success: true, filename: 'test.jpg', ts: Math.random()*99999999})
+	// return;
 	cam.capture(function(err, filename, timestamp) {
 		if(!err) {
       res.json({success: true, filename: filename, ts: timestamp});
